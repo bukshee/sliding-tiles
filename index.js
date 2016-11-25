db10ae7a1c1a845180394b29ff89152dcb17562c
Bukshee <adszabo@emis.com>
2016-11-25 20:33:07 +0100

    added: choose size

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ var s = document.createElement('script');
 s.src = hasES6?'src/sliding.js':'src/sliding.es5.js';
 document.head.appendChild(s);
 
+function start(ev) {
+  var s = document.querySelector('select');
+  var dim = s.options[s.selectedIndex].value.split('x');
+  SlidingTiles('#SlidingGame', dim[0], dim[1]);
+  if (ev) {
+    ev.preventDefault();
+  }
+  return false;
+}
+
 window.onload = function() {
+  //start();
   SlidingTiles('#SlidingGame');
+  //document.querySelector('button').addEventListener('click', start);
 };
 
